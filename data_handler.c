446f8ed9aada867b33885866ca338dd5ce17864e
Habeba Mahmoud <habebamahmoud882@gmail.com>
2025-07-22 19:04:50 +0300

    Updated data handler to load and save books

--- data_handler.c
+++ data_handler.c
@@ -1,6 +1,46 @@
-// القائمة الرئيسية والتوجيه (Main Menu)
-#include <stdio.h>
 
-void mainMenu();
-void adminMenu();
-void userMenu();
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data_handler.h"
+
+#define DATA_FILE "books.txt"
+
+void load_books(Book books[], int *count) {
+    FILE *file = fopen(DATA_FILE, "r");
+    if (!file) {
+        *count = 0;
+        return;
+    }
+
+    *count = 0;
+    while (fscanf(file, "%d|%[^|]|%[^|]|%d|%d\n",
+                  &books[*count].id,
+                  books[*count].title,
+                  books[*count].author,
+                  &books[*count].publication_year,
+                  &books[*count].is_borrowed) == 5) {
+        (*count)++;
+    }
+
+    fclose(file);
+}
+
+void save_books(const Book books[], int count) {
+    FILE *file = fopen(DATA_FILE, "w");
+    if (!file) {
+        printf("Error saving books!\n");
+        return;
+    }
+
+    for (int i = 0; i < count; i++) {
+        fprintf(file, "%d|%s|%s|%d|%d\n",
+                books[i].id,
+                books[i].title,
+                books[i].author,
+                books[i].publication_year,
+                books[i].is_borrowed);
+    }
+
+    fclose(file);
+}
--- data_handler.h
+++ data_handler.h
@@ -1,1 +1,9 @@
-# library-project
+#ifndef DATA_HANDLER_H
+#define DATA_HANDLER_H
+
+#include "book.h"
+
+void load_books(Book books[], int *count);
+void save_books(const Book books[], int count);
+
+#endif
