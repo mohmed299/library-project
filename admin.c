0026f5fa9a6177b2ef487ad6878fd4fc9bd62820
Habeba Mahmoud <habebamahmoud882@gmail.com>
2025-07-22 18:34:59 +0300

    Added admin view functions

--- admin.c
+++ admin.c
@@ -4,3 +4,52 @@
 void addBook();
 void removeBook();
 void countTotalBooks();
+    
+
+
+
+
+//view all books and view borrowed books
+#include "book.h"
+#include "admin.h"
+
+void view_all_books(const Book books[], int count) {
+    if (count == 0) {
+        printf(" No books in the library.\n");
+        return;
+    }
+
+    printf(" All Books in the Library:\n");
+    printf("--------------------------------------------------\n");
+
+    for (int i = 0; i < count; i++) {
+        printf("ID: %d\nTitle: %s\nAuthor: %s\nYear: %d\nBorrowed: %s\n\n",
+               books[i].id,
+               books[i].title,
+               books[i].author,
+               books[i].publication_year,
+               books[i].is_borrowed ? "Yes" : "No");
+    }
+}
+
+void view_borrowed_books(const Book books[], int count) {
+    int found = 0;
+
+    printf(" Borrowed Books:\n");
+    printf("--------------------------------------------------\n");
+
+    for (int i = 0; i < count; i++) {
+        if (books[i].is_borrowed) {
+            found = 1;
+            printf("ID: %d\nTitle: %s\nAuthor: %s\nYear: %d\n\n",
+                   books[i].id,
+                   books[i].title,
+                   books[i].author,
+                   books[i].publication_year);
+        }
+    }
+
+    if (!found) {
+        printf(" No books are currently borrowed.\n");
+    }
+}
--- admin.h
+++ admin.h
@@ -1,5 +1,13 @@
 // عرض كل الكتب والكتب المستعارة (Admin View)
-#include < <stdio.h>
+#ifndef ADMIN_H
+#define ADMIN_H
 
-void viewAllBooks();
-void viewBorrowedBooks();
+#include <stdio.h>
+#include "book.h"
+
+// Admin Function 2: View all books and borrowed books
+void view_all_books(const Book books[], int count);
+void view_borrowed_books(const Book books[], int count);
+
+#endif
+
