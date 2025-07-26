dfdb50ed47659df7e6bd0749dbfd8e9b0323d3f6
Mohamed Nabil Hashem <mohamedn888@yahoo.com>
2025-07-26 12:49:01 +0300

    Adding user borrowing functions

--- user.h
+++ user.h
@@ -7,6 +7,7 @@
 #include "book.h"
 #include "user.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void viewAvailableBooks();
 void borrowBook();
@@ -14,5 +15,8 @@ void returnBook();
 void searchByTitle();
 void filterByAuthor();
 void filterByYear();
+void viewAvailableBooks();
+void borrowBook();
+void returnBook();
 
 #endif
