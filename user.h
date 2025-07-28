2442b122e160873f9c06a8e1786817d8dbf2ac59
mohmed299 <jbrm693@gmail.com>
2025-07-27 22:49:33 -0800

    Update user.h

--- user.h
+++ user.h
@@ -1,13 +1,9 @@
-// استعارة، إرجاع، عرض الكتب المتاحة (User Borrowing)
-#ifndef DATA_HANDLER_H
-#define DATA_HANDLER_H
 
-#define DATA_FILE "books.txt"
+// user.h
+#ifndef USER_H
+#define USER_H
 
 #include "book.h"
-#include "user.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 void viewAvailableBooks();
 void borrowBook();
@@ -15,8 +11,5 @@ void returnBook();
 void searchByTitle();
 void filterByAuthor();
 void filterByYear();
-void viewAvailableBooks();
-void borrowBook();
-void returnBook();
 
 #endif
