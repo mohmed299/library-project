2c47586a5d27a979cef15bd846a267aaf97d7b78
mohmed299 <jbrm693@gmail.com>
2025-07-31 08:17:54 -0800

    Update book.h

--- book.h
+++ book.h
@@ -1,29 +1,22 @@
 #ifndef BOOK_H
 #define BOOK_H
-#include <string.h>
-#include <limits.h>
 
-#define MAX_TITLE_LEN 70
-#define MAX_AUTHOR_LEN 70
-#define MAX_BOOKS 500 
 
+#define MAX_TITLE_LEN 100
+#define MAX_AUTHOR_LEN 100
+#define MAX_BOOKS 1000
+
+// Define the Book structure
 typedef struct {
     int id;
     char title[MAX_TITLE_LEN];
     char author[MAX_AUTHOR_LEN];
     int publication_year;
-    int is_borrowed; 
+    int is_borrowed;
 } Book;
 
-
 void display_book(const Book *book);
 int get_next_id(const Book *books, int count);
-void clear_input_buffer(); 
-void display_book(const Book *book);
-int get_next_id(const Book *books, int count);
-void addBook();
-void removeBook();
-void countTotalBooks();
-int find_book_by_id(const Book *books, int count, int id);
+void clear_input_buffer();
 
-#endif
+#endif // BOOK_H
