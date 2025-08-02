573248882119f25fdc8a81e3dbf5ed951148f201
mohmed299 <jbrm693@gmail.com>
2025-08-02 14:10:21 -0800

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,18 +1,22 @@
-// عرض كل الكتب والكتب المستعارة (Admin View)
 #ifndef ADMIN_H
 #define ADMIN_H
 
 #include <stdio.h>
 #include "book.h"
 
-// fynction to find book by ID
-int find_book_by_id(const Book *books, int count, int id);
-
-void admin_mode(Book library[], int *book_count);
-// Admin Function 2: View all books and borrowed books
+// Function declarations only
+int find_book_by_id(int id);
+void addBook();
+void removeBook();
+void countTotalBooks();
+void admin_mode();
 void view_all_books(const Book books[], int count);
 void view_borrowed_books(const Book books[], int count);
+void invalid_choice();
 
+// Function to get library array address
+Book* get_library_address();
+int* get_book_count_address();
+int* get_next_id_address();
 
 #endif
-
