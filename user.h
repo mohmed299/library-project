85be7f8e805dc3acf2f901be840f21b846eb92a8
mohmed299 <jbrm693@gmail.com>
2025-08-02 14:06:32 -0800

    Update user.h

--- user.h
+++ user.h
@@ -1,15 +1,19 @@
-// user.h
 #ifndef USER_H
 #define USER_H
 
-
 #include "book.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+// Function declarations only (no definitions in header)
 void viewAvailableBooks();
 void borrowBook();
 void returnBook();
 void searchByTitle();
 void filterByAuthor();
 void filterByYear();
+void toLowerStr(char *str);
+void init_user_pointers();
 
 #endif
